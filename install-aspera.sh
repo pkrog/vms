234c79f79694be7a19eb275ec3f57d12cc830f10
Pierrick Roger <pk.roger@icloud.com>
2019-04-18 17:28:14 +0200

    Remove old vms.

--- install-aspera.sh
+++
@@ -1,4 +0,0 @@
-apk add openssl wget
-wget https://download.asperasoft.com/download/sw/cli/3.7.7/aspera-cli-3.7.7.608.927cce8-linux-64-release.sh
-bash aspera-cli-3.7.7.608.927cce8-linux-64-release.sh
-rm aspera-cli-3.7.7.608.927cce8-linux-64-release.sh
--- vm-phn-docker-pwiz-gentoo/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo emerge --sync
-sudo emerge app-emulation/docker
-emerge -uDU --with-bdeps=y git
-mkdir dev
-cd dev
-git clone --recursive https://github.com/phnmnl/docker-pwiz
